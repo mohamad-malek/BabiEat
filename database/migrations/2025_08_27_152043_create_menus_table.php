64f48341470c0c99d204d627a96723a0e6cf6e85
mohamad malek <mohamadmalek@gmail.com>
2025-08-27 18:58:59 +0300

    som fixes

---
+++ database/migrations/2025_08_27_152043_create_menus_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('menus', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('branch_id')->constrained('branches')->cascadeOnDelete();// if branch is dleted remove its menu
+            $table->string('name'); // name of menu breakfast launch  main menu  etc  ...
+            $table->boolean('is_available')->default(1);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('menus');
+    }
+};
